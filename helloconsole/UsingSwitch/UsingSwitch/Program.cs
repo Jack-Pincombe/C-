067f05c83f133a851e91b5bd77d61ea8f8fe9293
Jack-Pincombe <jackpincombe1@gmail.com>
2017-07-08 19:29:19 +0100

    using the switch capabilites along side with creating a method which can call itself

---
+++ helloconsole/UsingSwitch/UsingSwitch/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace UsingSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Xyz();
+
+    
+
+            void Xyz()
+            {
+
+                Console.WriteLine("Enter an operation: + - * /");
+                string operation = Console.ReadLine();
+
+                switch (operation)
+                {
+
+                    case "+":
+                        Console.WriteLine("You chose to add");
+                        break;
+
+                    case "-":
+                        Console.WriteLine("You chose to substract");
+                        break;
+
+                    case "/":
+                        Console.WriteLine("You chose to divide");
+                        break;
+
+                    case "*":
+                        Console.WriteLine("You chose to multiply");
+                        break;
+
+                    default:
+                        break;
+                }
+
+                Console.WriteLine("Would you like to go again?");
+                string inp = Console.ReadLine();
+                if (inp == "y")
+                {
+                    Xyz();
+                }
+                else
+                {
+                    Console.Beep();
+                }
+
+                    
+
+            }
+
+
+            
+        }
+    }
+}
