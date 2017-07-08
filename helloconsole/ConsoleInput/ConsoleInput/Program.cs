72ca7e6a45e8c381ca1a9ab0516f0072895d2afa
Jack-Pincombe <jackpincombe1@gmail.com>
2017-07-08 19:04:09 +0100

    Taking the users input and counting to it using sleep and trypars

--- helloconsole/ConsoleInput/ConsoleInput/Program.cs
+++ helloconsole/ConsoleInput/ConsoleInput/Program.cs
@@ -6,8 +6,26 @@ namespace ConsoleInput
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.Read();
+            Console.WriteLine("Please enter the amount of numbers to count");
+            string x = Console.ReadLine();
+            int y;
+
+            if(Int32.TryParse(x, out y))
+            {
+                for (int i = 0; i < y; i++)
+                {
+                    Console.WriteLine(i);
+                    System.Threading.Thread.Sleep(50);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please ensure you enter a valid number into the console ");
+                Console.Read();
+            }
+                
+
+            
         }
     }
 }
