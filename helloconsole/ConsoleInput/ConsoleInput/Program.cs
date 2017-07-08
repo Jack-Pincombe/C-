f16b8ad8c5820c32a0ceefe7f648dccde3ad534e
Jack-Pincombe <jackpincombe1@gmail.com>
2017-07-08 18:53:46 +0100

    |learning about inputs into the console

---
+++ helloconsole/ConsoleInput/ConsoleInput/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Read();
+        }
+    }
+}
