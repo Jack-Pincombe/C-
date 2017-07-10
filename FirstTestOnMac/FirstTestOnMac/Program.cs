366ab86ca5f05779ec6ee28d2672ac351cbf1d2d
Jack Pincombe <jackpincombe1@gmail.com>
2017-07-10 16:38:06 +0100

    first commiting c# from the mac

---
+++ FirstTestOnMac/FirstTestOnMac/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstTestOnMac
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Mac C#");
+        }
+    }
+}
