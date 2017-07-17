359c9414b26e07deb38f0055d91e87a46727a9b0
Jack-Pincombe <jackpincombe1@gmail.com>
2017-07-17 21:29:34 +0100

    learning about how to use try and catch similar to java

---
+++ HelloConsole/ConsoleAppTry/ConsoleAppTry/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleAppTry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+                {
+                    string[] fruit = { "Apples", "Oranges", "Pineapples" };
+                for (int i = 0; i <= fruit.Length; i++)
+                {
+                    Console.WriteLine("The fruit is " + fruit[i]);
+                }
+                Console.ReadKey();
+                }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+         
+        }
+    }
+}
